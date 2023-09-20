cb8e30134a866dd96e78fc39a37aad935bc57762
mobasshirCode <mobasshir.code@gmail.com>
2023-09-21 03:27:13 +0530

    Added news cards

--- src/components/News.js
+++ src/components/News.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is a News container.
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container">
+        <h3>News Reaper - Top Headlines</h3>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
+    let {title,description} = this.props;
     return (
       <div>
-        This is a News Item.
+        <div className="card" style={{width: '18rem'}}>
+          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
