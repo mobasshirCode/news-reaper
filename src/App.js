b357de31931c1a1d779f8dcc8aff101a8a47c180
mobasshirCode <mobasshir.code@gmail.com>
2023-09-21 02:11:00 +0530

    Added basic Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import "./App.css";
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        This is my first React app using class based components.
-      </div>
-    )
+      <>
+        <Navbar/>
+      </>
+    );
   }
 }
-
