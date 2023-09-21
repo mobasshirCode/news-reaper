2acce77dbc84a903a5d8078ac7cff52d69dae425
mobasshirCode <mobasshir.code@gmail.com>
2023-09-21 07:16:27 +0530

    Fetch data from local sample

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let {title,description} = this.props;
+    let {title,description,imageUrl,newsUrl} = this.props;
     return (
       <div>
         <div className="card" style={{width: '18rem'}}>
-          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title}...</h5>
             <p className="card-text">
-              {description}
+              {description}...
             </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <a href={newsUrl} target="_blank" className="btn btn-primary">
+              Read more
             </a>
           </div>
         </div>
