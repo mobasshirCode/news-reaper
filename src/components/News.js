c068dd6d150929ef4c0dc3fa1d96bc30160008eb
mobasshirCode <mobasshir.code@gmail.com>
2023-09-23 00:19:41 +0530

    Added time and source

--- src/components/News.js
+++ src/components/News.js
@@ -12,14 +12,14 @@ export default class News extends Component {
     };
   }
   async componentDidMount() {
-    this.setState({loading : true})
+    this.setState({ loading: true });
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=8872901d422b46ccaff1d40589029842&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading :false
+      loading: false,
     });
   }
   // handlePrev = () => {
@@ -37,7 +37,9 @@ export default class News extends Component {
   //   );
   // };
   handlePrev = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=8872901d422b46ccaff1d40589029842&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${
+      this.props.category
+    }&apiKey=8872901d422b46ccaff1d40589029842&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
@@ -45,36 +47,48 @@ export default class News extends Component {
     this.setState({ articles: parsedData.articles, page: this.state.page - 1 });
   };
   handleNext = async () => {
-    this.setState({loading : true})
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=8872901d422b46ccaff1d40589029842&page=${
+    this.setState({ loading: true });
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${
+      this.props.category
+    }&apiKey=8872901d422b46ccaff1d40589029842&page=${
       this.state.page + 1
     }&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles, page: this.state.page + 1 ,loading : false });
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page + 1,
+      loading: false,
+    });
   };
   render() {
     return (
       <div className="container my-2">
-        <h1 className="text-center" style={{margin : "30px"}}>News Reaper - Top Headlines</h1>
-        {this.state.loading && <Spinner/>}
+        <h1 className="text-center" style={{ margin: "30px 0" }}>
+          News Reaper - Top Headlines
+        </h1>
+        {this.state.loading && <Spinner />}
         <div className="row my-3 ">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4 my-3" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title.slice(0, 45) : ""}
-                  description={element.description ? element.description : ""}
-                  newsUrl={element.url}
-                  imageUrl={
-                    element.urlToImage
-                      ? element.urlToImage
-                      : "https://thumbs.dreamstime.com/b/newspaper-wooden-table-93401143.jpg"
-                  }
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4 my-3" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={element.description ? element.description : ""}
+                    newsUrl={element.url}
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://thumbs.dreamstime.com/b/newspaper-wooden-table-93401143.jpg"
+                    }
+                    author={element.author ? element.author : "Unknown"}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="d-flex justify-content-between">
           <button
@@ -87,7 +101,8 @@ export default class News extends Component {
           </button>
           <button
             disabled={
-              this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
             }
             type="button"
             className="btn btn-dark"
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,32 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div>
-        <div className="card" >
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card">
+          <span
+            className="position-absolute top-0  translate-middle badge rounded-pill bg-success"
+            style={{ left: "90%", zIndex: 1 }}
+          >
+            {source}
+          </span>
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
             <p className="card-text">
-              {description}...
+              <small className="text-body-secondary">
+                By {author} on {new Date(date).toLocaleString()}
+              </small>
             </p>
-            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-dark"
+            >
               Read more
             </a>
           </div>
