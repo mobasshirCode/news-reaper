6075e3cd74e5537c2f550a14205cbbc3591421f5
mobasshirCode <mobasshir.code@gmail.com>
2023-09-22 21:04:39 +0530

    Added react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export class Navbar extends Component {
 
@@ -7,42 +8,40 @@ export class Navbar extends Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">News Reaper</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
-          </li>
-          <li className="nav-item">
-          <a className="nav-link" href="/about">Business</a></li>
-          <li className="nav-item">
-          <a className="nav-link" href="/about">Entertainment </a></li>
-          <li className="nav-item">
-          <a className="nav-link" href="/about">General </a></li>
-          <li className="nav-item">
-          <a className="nav-link" href="/about">Health </a></li>
-          <li className="nav-item">
-          <a className="nav-link" href="/about">Science </a></li>
-          <li className="nav-item">
-          <a className="nav-link" href="/about">Sports </a></li>
-          <li className="nav-item">
-          <a className="nav-link" href="/about">Technology</a></li>
+          <div className="container-fluid">
+            <Link className="navbar-brand" to='/'>News Reaper</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/">Home</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/business">Business</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/entertainment">Entertainment </Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/general">General </Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health">Health </Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/science">Science </Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/sports">Sports </Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/technology">Technology</Link></li>
 
-       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+              </ul>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
       </div>
     )
   }
